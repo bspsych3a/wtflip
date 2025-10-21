2b5c1bac0a786ac239b757a9ed932ccddfc95ba4
Christopher Ped <231029324+bspsych3a@users.noreply.github.com>
2025-10-21 19:15:15 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,7 +61,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cd30af31f5e6374abe3eaa40796215da
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "582345730155daf1eeb36a7590942db5",
+"flutter_bootstrap.js": "2e5db1edd75da9c266665f01945df752",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
